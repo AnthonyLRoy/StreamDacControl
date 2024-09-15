45dd28fff3985c0a627489618efbfedd56aeb169
AnthonyLRoy <Anthonylroy@hotmail.com>
2024-09-15 13:00:16 +0100

    fix monitor

--- src/main.cpp
+++ src/main.cpp
@@ -22,40 +22,65 @@ void main::run()
 
     printf(">I2Cbus Example \n");
     fflush(stdout);
-    I2C_t& myI2C = i2c0;  // i2c0 and i2c1 are the default objects
-    gpio_set_direction(GPIO_NUM_48,GPIO_MODE_OUTPUT);
-    gpio_set_level(GPIO_NUM_48,1);
-    ESP_ERROR_CHECK( myI2C.begin(GPIO_NUM_17, GPIO_NUM_18, 100000));
+    I2C_t &myI2C = i2c0; // i2c0 and i2c1 are the default objects
+    gpio_set_direction(GPIO_NUM_48, GPIO_MODE_OUTPUT);
+    gpio_set_level(GPIO_NUM_48, 1);
+    ESP_ERROR_CHECK(myI2C.begin(GPIO_NUM_16, GPIO_NUM_15, 50000));
     myI2C.setTimeout(10);
     myI2C.scanner();
-    esp_err_t r = myI2C.testConnection(0x20,1000);
- 
-    uint8_t buffer[1] ={ 0x1};
-    myI2C.writeByte(MCP23017_ADDRESS,MCP23017_IODIRA,0x00);
-    myI2C.writeByte(MCP23017_ADDRESS,MCP23017_IODIRB,0x00);
+    esp_err_t r = myI2C.testConnection(0x20, 1000);
 
+    uint8_t buffer[1] = {0x1};
+    myI2C.writeByte(MCP23017_ADDRESS, MCP23017_IODIRA, 0x00);
+    myI2C.writeByte(MCP23017_ADDRESS, MCP23017_IODIRB, 0x00);
 
-    myI2C.writeByte(MCP23017_ADDRESS,MCP23017_OLATB,0xff);
-    myI2C.writeByte(MCP23017_ADDRESS,MCP23017_OLATA,0xff);
+    myI2C.writeByte(MCP23017_ADDRESS, MCP23017_OLATB, 0xff);
+    myI2C.writeByte(MCP23017_ADDRESS, MCP23017_OLATA, 0xff);
 
-    myI2C.writeByte(MCP23017_ADDRESS,MCP23017_IODIRA,0x00);
-    myI2C.writeByte(MCP23017_ADDRESS,MCP23017_IODIRB,0x00);
+    myI2C.writeByte(MCP23017_ADDRESS, MCP23017_IODIRA, 0x00);
+    myI2C.writeByte(MCP23017_ADDRESS, MCP23017_IODIRB, 0x00);
 
-    uint8_t cnt =0;
-    while (1) {
-   gpio_set_level(GPIO_NUM_48,1);
+    uint8_t cnt = 0;
+    while (1)
+    {
+        gpio_set_level(GPIO_NUM_48, 1);
 
         vTaskDelay(1000 / portTICK_PERIOD_MS);
-    myI2C.writeByte(MCP23017_ADDRESS,MCP23017_OLATB,0xff);
-    myI2C.writeByte(MCP23017_ADDRESS,MCP23017_OLATA,0xff);
-     gpio_set_level(GPIO_NUM_48,0);
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-              gpio_set_level(GPIO_NUM_48,1);   
-        myI2C.writeByte(MCP23017_ADDRESS,MCP23017_OLATB,0x00);
-    myI2C.writeByte(MCP23017_ADDRESS,MCP23017_OLATA,0x00); 
-        
+        myI2C.writeByte(MCP23017_ADDRESS, MCP23017_OLATB, 0xff);
+        myI2C.writeByte(MCP23017_ADDRESS, MCP23017_OLATA, 0xff);
+
+        esp_err_t res = myI2C.readByte(0x20, MCP23017_IOCONA, buffer);
+        if (res != ESP_OK)
+        {
+            ESP_LOGW("FAIL", "READ FAILED %s ", esp_err_to_name(res));
+        }
+        else
+        {
+            std::string header = std::to_string(buffer[0]);
+            ESP_LOGI("result", "RESULT A IS : %s", header.c_str());
+        }
+vTaskDelay(100 / portTICK_PERIOD_MS);
+        esp_err_t res2 = myI2C.readByte(0x20, MCP23017_IOCONB, buffer);
+        if (res != ESP_OK)
+        {
+            ESP_LOGW("FAIL", "READ FAILED %s ", esp_err_to_name(res2));
+        }
+        else
+        {
+            std::string header = std::to_string(buffer[0]);
+            ESP_LOGI("result", "RESULT B IS : %s", header.c_str());
+        }
+
+        gpio_set_level(GPIO_NUM_48, 0);
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+
+        gpio_set_level(GPIO_NUM_48, 1);
+
+        myI2C.writeByte(MCP23017_ADDRESS, MCP23017_OLATB, 0x00);
+        myI2C.writeByte(MCP23017_ADDRESS, MCP23017_OLATA, 0x00);
+
         // ESP_LOGI("Test","testing Address 0x%X ", cnt);
-        
+
         // esp_err_t res = myI2C.readByte(0x20,cnt++, buffer);
         // if (res != ESP_OK)
         // {
@@ -66,11 +91,10 @@ void main::run()
         // vTaskDelay(1000 / portTICK_PERIOD_MS);
         // if (cnt  > 21) {cnt = 0;};
     }
-    
+
     myI2C.close();
     vTaskDelay(portMAX_DELAY);
 };
-
 
 // extern "C" void app_main() {
 
@@ -78,7 +102,7 @@ void main::run()
 //     fflush(stdout);
 
 //     I2C_t& myI2C = i2c0;  // i2c0 and i2c1 are the default objects
-    
+
 //     ESP_ERROR_CHECK( myI2C.begin(GPIO_NUM_17, GPIO_NUM_18, 100000));
 //     myI2C.setTimeout(10);
 //     myI2C.scanner();
@@ -91,7 +115,7 @@ void main::run()
 //        esp_err_t r = myI2C.testConnection(0x20,1000);
 //        ESP_LOGI("connection","TEST RESULT %s",esp_err_to_name(r));
 //         ESP_LOGI("Test","testing Address");
-        
+
 //         esp_err_t res = myI2C.readBytes(0x20, 0x12, 1, buffer);
 //         if (res != ESP_OK)
 //         {
@@ -102,7 +126,7 @@ void main::run()
 //         vTaskDelay(1000 / portTICK_PERIOD_MS);
 
 //     }
-    
+
 //     myI2C.close();
 //     vTaskDelay(portMAX_DELAY);
 // }
